5b5459e05ced17db47b1ced36bc47f57dd64c0e9
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-05-16 20:22:30 +0530

    Initial commit

---
+++ present.js
@@ -0,0 +1,171 @@
+var to = 'Jayesh 😺❤️';
+var gift_url = 'http://www.amazon.com/gp/product/B00X4WHP5E/ref=ods_xs_ae_shurl?tag=googhydr-20&hvadid=74652194316&hvpos=1t1&hvexid=&hvnetw=g&hvrand=7560558939874219351&hvpone=&hvptwo=&hvqmt=b&hvdev=c&ref=pd_sl_2vatdqwe3p_b';
+var gift_image_url = 'https://www.intouchsol.com/Content/Libraries//echo-blog-body.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+var ans = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
+
+var reviews = [
+  `It started with a “Hi”, I never thought for a second that it would get to the point where you would become my great friend. 
+  You are so far away from me, and I know I might never get the opportunity to meet you. But do know that you always live in my heart. Thanks for so much of love and care. Happy birthday.
+  May this year bring lots and lots of happiness and joy.`,
+  `A birthday is a good time to remember our friends👫.Be crazy, laugh & live it this because this is the oldest U have been and the youngest U will ever be again 🥰HAPPIEST B'DAY MR.KUKREJA👻👻🎂🎉🥳may all ur dreams come true🥂stay home and stay safe😇Happy quarantine bday Jayesh.😆🎉🥳`,
+  `Happy birthday jashi gbu Watching you grow up over the years has been one of my great joys. I’m so privileged to be a part of your life, and I couldn’t have asked for a better brother to share my years with.`,
+  `Wish you happy lockdown wala birthday bro....gbu always may u got more titliya 🦋😆🤣 in your life..thanks for helping me 😗 hope you have an amazing day I'll find soon a beautifull dewarani for me 🤭हुकुम चलाऊंगी मैं
+  आँख दिखाउंगी मैं
+  सेहमी खड़ी रहेगी मेरी देवरानी 🎊♥️
+  Enjy your day I phone jldi h mil jayega 🤭🤣😂🎊🎂 jugaad chalu h 👍 tere gift or party ki demand after lockdown 🤣🤣 n vote mujhe h dena ok`,
+  `Hii Jazz!! The joy is in the air because your special day is here!Hey my akduuu frnd today is your day🥳🥳💖 Wishing you happiest quarantine vala birthday 🎂🎈 Tum jiyo hazaarooo saal yeh hamaraii h aarazuuuu😛 Happy birthday to you......❤❤`,
+  `Firstly wishing you a very happy quarantine birthday...❤️❤️ thank you so much for always being there whenever I needed...😬🤭 I so miss a call from you saying ki ready hoja Bs aarha hu chai peene chl rhe h😟I wish tujhse Koi or kabhi na pattey🤣u can't live without your overacting jo srf do he log Smjh skte h😬🕉️Tere sath aisa lgta he nhi h ki srf do saal hue hai aisa lgta h bchpan se sath h😚you are my jai and I'm your veeru❤️ You are such a wonderful friend that needs no description❤️🥰Hope many more blessings will follow you in the coming years..😚🥰`,
+  `Hello bestfriend 😇 
+  Happy birthday 🥳❣ 
+  I say our friendship is weird 🤯
+  We tease each other 🤪
+  We argue and disagree all the time 😤
+  But still can’t live without each other 😚 
+  Guess who am I❓
+  Hint - we play role of famous cartoon character 🤭`,
+  `Remember when we used to fight for tv remote in front of our grandparents u always used to win it as u were the younger one they used to support u and pamper u more and u always used to tease me about it that u are the favourite child i miss those days alot wish could live them again with u.
+  Not only in childhood still we rock every place we go especially the weddings and family functions i love sharing one plate with and commenting and judging people it is always fun with u and always will be as I'm not leaving u alone ever love u more than anything else
+  One more thing as you become another year older I don’t want you to worry. It will always be my job to annoy you no matter how old we get.
+  Happy Birthday 🥳❤️
+  Love you💋❣️`,
+  `Happy Birthday KS`,
+  `We may not share the same space anymore but we have shared some of the most cutest memories in our lives together. Our friendship will grow stronger every time we think of them😄..and lastly happiest birthday jayesh enjoy your day being at home by spending time with your family 💕`,
+  `Maybe I am not telling it so often, but I value our friendship.Friends like you make life special!
+  May your birthday bring you as much happiness as you  give to everyone who knows you Happy Birthday! Jayeshaaa😇🥳`,
+  `Happy Birthday BC 💥✨
+  Teko guess karna hai na kiska msg hai yeh karte reh tu guess malum pad gya toh bohot achha nhi toh ab tak kya kiya tha jo aage karega😂😂😂😂`,
+  `Hello jayesshu Bha Happy Birthday 🥳 Matarani Bless You💓🌈  Humesha Aise hi rehna Khubbb taraki Karo 😋😛 Goodbye and Takecare😇😝`,
+  `school se yaari hai apni,
+  Gk p sath tha tera,
+  manish se dusmani si gheri,
+  share karta tu saari baate apni
+  aisi hai yaari apni.
+  
+  chal tuje ek hint deta hu guess karne k liye teri twister ki gadi se maine pehele bike chalana sikhi thi.`,
+  `Wishing you a many many happy returns of the day Jayesh. May ur dreams come true. God bless u as always Have a joyful health journey ahead
+
+  I totally agree on the fact that, we both are different sides of same coin😂
+  
+  Tu left jyega to mein right
+  
+  Jaise 9 bje ki class k liye me ghr 8.30 choduga to tu 8.30 uthega😂
+  
+  Mein Marks score Karne m acha kr raha to tu life m acha kr raha 
+  Ye marks kahi kaam aye ya na aye 
+  lekin tu jis trah har kisi s nibhata wo jarur  kaam ayega
+  
+  
+  Here are the few  things jo mere life m sirf aur sirf tere vjh se hui hai. And i m totally thankful to u agar ye sab cheze mere lyf m nai hoti to meri life khichdi jaise hoti ekdum boring🤷🏻‍♂️😂
+  1)Nightouts:- Bhale shuru majburi m kiye the ppt banane k liye lekn baad m lekn dhire dhire maza krne lage
+  
+  2)Bike Ride to marine drive
+  
+  3)After Exams long bike rides teen jhadi ya lake tak
+  
+  4)And most importantly Engineering bhaai sahab😂
+  
+  Thankyou So much for always being there for me wo bdays p logo ko jama krke cake
+  
+  cake cutting  ho ya meri gaadi kahi bch m atak gayi ho😂
+  
+  being a brother by blood u r more thn a brother by heart
+  
+  Cheers to all these years mere bhai🍻
+  Wishing u all the very best for future. 
+  
+  P.S:- You are already blessed bcz u have such kind and geniune friends like FAB 5  in this era😇`,
+  `Sachche Dost ki 3 Nishaniyan:
+  1) Kamina kabhi phone nahi karenge.Aur agar krega b toh bolega tune phone kiya kya jo mai kru
+  2) Jabi millega hamesha bolega yeh ladki se setting krvake dee
+  3) hamesha juth bolna`,
+  `You are truly a blessing to us! We are happy every day to have you as our Brother👑. Happy birthday and always remember that we love you forever.💓🌈`,
+  `You are sweetest 😋person i have meet till date 🙂
+  A person who is always there for me 🤝
+  A person who always takes care that I m fine..🤩 
+  A person who knows how my mood🧐 is just by reading my msgs 📖
+  A person who never shows anyknid of attitude 😎and be there for me even when we fight.🤜🤛
+  Thank you so much❤ humesha sath Rehne k leye..🤝🤝 
+  Humesha meko samajhne k leye..🥺 
+  Thank you so much for everything ❤❤
+  Wish you a happiest🥰 home arrest😜 wala bdayyyy 🥳🥳
+  May you get everything you wish and desire❤ 
+  Always keep smile.🙂.. The way you try to keep everyone smiling around you.. ❤❤`
+];
+var last_person_msg = `Heeeyyaaaaa Bandhu👀💓 Janam dhia jyu Lakha  Lakha Karor Karor vadayu🌝 🥳💓
+When I think back to all things we've been through, I am so thankful🌈 that I got to go through everything with you by my side👫🏻you'll never have to walk the world alone. As long as my heart beats,👀you'll have me💞💓
+I've always got your Back and you Mine and u know this😘👀 And it's not the  Number of Friends, It's the quality of them😍 Thanks for being the best of the best🌈👑 Humesha Aise hi rehna Mr.K don't change🤨 or else Laffeeyy khaega😼😽 Happy Birthday Laddoooo💓👀 May God bless you with all the Happiness in the World May your all Dreams come true💓💓 Love You the Most😘`;
+var already_done = [];
+
+function init() {
+
+  present.addEventListener("click", function (e) {
+    document.getElementById("present_text").style.display = "block";
+    if (already_done.length != ans.length) {
+      random_num = Math.floor((Math.random() * reviews.length));
+      console.log(random_num);
+      if (already_done.includes(random_num)) {
+        do {
+          console.log(random_num);
+          random_num = Math.floor((Math.random() * reviews.length));
+        } while (already_done.includes(random_num));
+      }
+      already_done.push(random_num);
+
+      present.classList.add("open");
+      console.log(ans[random_num]);
+      document.getElementById("guess_img").src = "images/posts/" + ans[random_num] + ".jpg";
+      setTimeout(function () {
+        document.getElementById("questions").style.visibility = "visible";
+        document.getElementById("questions").style.opacity = 1;
+        document.getElementById("questions").style.display = "block";
+      }, 300);
+    } else if (random_num == reviews.length + 1) {
+      window.alert("Bas Kar na aur kitni tariff sunni h 😂");
+    } else {
+      //if you want certian person to appear at the end 
+      random_num = reviews.length + 1;
+      document.getElementById("guess_img").src = "images/posts/last.jpg";
+      setTimeout(function () {
+        document.getElementById("questions").style.visibility = "visible";
+        document.getElementById("questions").style.opacity = 1;
+        document.getElementById("questions").style.display = "block";
+      }, 300);
+    }
+  }, true);
+
+  document.getElementById("view_answer").addEventListener("click", function (e) {
+    document.getElementById("reviewer_name_with_jayesh").src = "images/pics_with_jayesh/" + (random_num == reviews.length + 1 ? "last" : ans[random_num]) + ".jpg";
+    // document.getElementById("reviewer_name").innerHTML = ans[random_num];
+    document.getElementById("review").innerHTML = (random_num == reviews.length + 1 ? last_person_msg : reviews[random_num]);
+
+    document.getElementById("questions").style.visibility = "hidden";
+    document.getElementById("questions").style.opacity = 0;
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("answers").style.visibility = "visible";
+    document.getElementById("answers").style.opacity = 1;
+    document.getElementById("answers").style.display = "block";
+  }, true);
+
+
+  document.getElementById("guess_another").addEventListener("click", function (e) {
+    document.getElementById("answers").style.visibility = "hidden";
+    document.getElementById("answers").style.opacity = 1;
+    document.getElementById("answers").style.display = "none";
+    document.getElementById("present_text").style.display = "none";
+    setTimeout(function () {
+      present.classList.remove("open");
+    }, 300);
+  }, true);
+
+
+  nametag.innerText = to;
+}
+
+init();
